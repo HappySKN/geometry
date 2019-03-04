30d42fab848c5f2ef0409bc2f4fca3decf258f54
HappySKN <kirill.avdeev2000@yandex.ru>
2019-03-04 20:11:40 +0700

    Now don't sends warning message

--- main.cpp
+++ main.cpp
@@ -6,15 +6,13 @@ void cir()
     int gd = DETECT, gm, n;
     printf("Enter quanity of circle: ");
     scanf("%d", &n);
-    int i, A[n], x[n], y[n], r[n];
-    for (i = 0; i < n; i++)
-    {
+    int i, x[n], y[n], r[n];
+    for (i = 0; i < n; i++) {
         printf("\nEnter x%d, y%d, r%d: ", i + 1, i + 1, i + 1);
         scanf("%d%d%d", &x[i], &y[i], &r[i]);
     }
     initgraph(&gd, &gm, NULL);
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         circle(x[i], y[i], r[i]);
     }
 }
